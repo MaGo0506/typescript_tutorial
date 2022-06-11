b683512596a2b7a60398b217039fe03c0e28e809
Marko Goronjic <marko.goronjic@quantox.com>
2022-06-11 20:28:32 +0200

    Interfaces

--- public/app.js
+++ public/app.js
@@ -1,3 +1,18 @@
+const me = {
+    name: 'marko',
+    age: 30,
+    speak(text) {
+        console.log(text);
+    },
+    spend(amount) {
+        console.log(`I spent ${amount}`);
+        return amount;
+    }
+};
+const greePerson = (person) => {
+    console.log(`hello`, person.name);
+};
+greePerson(me);
 import { Invoice } from "./classes/invoice.js";
 const invOne = new Invoice('Mario', 'Work on the Mario website', 250);
 const invTwo = new Invoice('Luigi', 'Work on the Luigi website', 350);
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,31 @@
+// Interfaces
+interface IsPerson {
+    name: string;
+    age: number;
+
+    speak(a: string): void;
+
+    spend(a: number): number
+}
+
+const me: IsPerson = {
+    name: 'marko',
+    age: 30,
+    speak(text: string): void {
+        console.log(text)
+    },
+    spend(amount: number): number {
+        console.log(`I spent ${amount}`);
+        return amount;
+    }
+};
+
+const greePerson = (person: IsPerson) => {
+    console.log(`hello`, person.name);
+}
+
+greePerson(me)
+
 import {Invoice} from "./classes/invoice.js";
 
 const invOne = new Invoice('Mario', 'Work on the Mario website', 250);
