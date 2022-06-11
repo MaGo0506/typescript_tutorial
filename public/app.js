92cf4fab875dbe1270c49c988b9a3f2d08821d31
Marko Goronjic <marko.goronjic@quantox.com>
2022-06-11 20:16:53 +0200

    Modules

--- public/app.js
+++ public/app.js
@@ -1,18 +1,4 @@
-"use strict";
-// Classes
-class Invoice {
-    // readonly client: string;
-    // private details: string;
-    // public amount: number;
-    constructor(client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-    }
-    format() {
-        return `${this.client} owes £${this.amount} for ${this.details}`;
-    }
-}
+import { Invoice } from "./classes/invoice.js";
 const invOne = new Invoice('Mario', 'Work on the Mario website', 250);
 const invTwo = new Invoice('Luigi', 'Work on the Luigi website', 350);
 let invoices = [];
@@ -20,18 +6,4 @@ invoices.push(invOne, invTwo);
 console.log(invoices);
 invoices.forEach(inv => {
     console.log(inv.client, inv.amount, inv.format());
-});
-// console.log(invOne.format());
-// console.log(invTwo.format());
-//
-// console.log(invOne)
-// console.log(invTwo)
-// const form = document.querySelector('form')!;
-const form = document.querySelector('.new-item-form');
-// console.log(form.children)
-// inputs
-const type = document.querySelector('#type'), toFrom = document.querySelector('#tofrom'), details = document.querySelector('#details'), amount = document.querySelector('#amount');
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
 });
